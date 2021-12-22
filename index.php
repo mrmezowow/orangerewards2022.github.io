f63c78cd220c2304d06e387eb7ba87708801a49f
mrmezowow <53160343+mrmezowow@users.noreply.github.com>
2021-12-22 17:28:34 +0200

    Update and rename index.md to index.php

---
+++ index.php
@@ -0,0 +1,322 @@
+<html class="wf-montserrat-n4-active wf-caveat-n4-active wf-active" lang="en"><head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  
+  
+  <meta property="og:title" content="orcm">
+  
+  <meta property="og:image" content="https://page.co/preview/396365/null/1639719363000">
+  <meta property="og:type" content="website">
+  <meta property="og:image:width" content="600">
+  <meta property="og:image:height" content="600">
+  <link rel="shortcut icon" href="https://cf.mailmunch.co/partner/mailmunch/favicon.png">
+  <title>orcm</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+  
+  <script src="https://a.mailmunch.co/app/v1/form.js"></script>
+  
+  <style>
+    a { color: #0000ee; text-decoration: underline; } a:hover { color: #0000ee; text-decoration: underline; } #u_content_button_2 a:hover { color: #FFFFFF !important; background-color: #3AAEE0 !important; } #u_content_button_3 a:hover { color: #FFFFFF !important; background-color: #3AAEE0 !important; } @media (max-width: 480px) { #u_content_image_1.v-container-padding-padding { padding: 0px !important; } #u_content_image_2 .v-src-width { width: 960px !important; } #u_content_image_2 .v-src-max-width { max-width: 90% !important; } }
+.u-row {
+  display: flex;
+  flex-wrap: nowrap;
+  margin-left: 0;
+  margin-right: 0;
+}
+
+.u-row .u-col {
+  position: relative;
+  width: 100%;
+  padding-right: 0;
+  padding-left: 0;
+}
+
+
+.u-row .u-col.u-col-100 {
+  flex: 0 0 100%;
+  max-width: 100%;
+}
+
+
+@media (max-width: 767px) {
+  .u-row:not(.no-stack) {
+    flex-wrap: wrap;
+  }
+
+  .u-row:not(.no-stack) .u-col {
+    flex: 0 0 100% !important;
+    max-width: 100% !important;
+  }
+}
+
+.layout-mobile .u-row:not(.no-stack) {
+  flex-wrap: wrap;
+}
+
+.layout-mobile .u-row:not(.no-stack) .u-col {
+  flex: 0 0 100% !important;
+  max-width: 100% !important;
+}
+body,html{padding:0;margin:0}html{box-sizing:border-box}*,:after,:before{box-sizing:inherit}html{font-size:14px;-ms-overflow-style:scrollbar;-webkit-tap-highlight-color:rgba(0,0,0,0)}body{font-family:Arial,Helvetica,sans-serif;font-size:1rem;line-height:1.5;color:#373a3c;background-color:#fff}p{margin:0}.error-field{-webkit-animation-name:shake;animation-name:shake;-webkit-animation-duration:1s;animation-duration:1s;-webkit-animation-fill-mode:both;animation-fill-mode:both}.error-field input,.error-field textarea{border-color:#a94442!important;color:#a94442!important}.field-error{padding:5px 10px;font-size:14px;font-weight:700;position:absolute;top:-20px;right:10px}.field-error:after{top:100%;left:50%;border:solid transparent;content:" ";height:0;width:0;position:absolute;pointer-events:none;border-color:rgba(136,183,213,0);border-top-color:#ebcccc;border-width:5px;margin-left:-5px}.spinner{margin:0 auto;width:70px;text-align:center}.spinner>div{width:12px;height:12px;background-color:hsla(0,0%,100%,.5);margin:0 2px;border-radius:100%;display:inline-block;-webkit-animation:sk-bouncedelay 1.4s infinite ease-in-out both;animation:sk-bouncedelay 1.4s infinite ease-in-out both}.spinner .bounce1{-webkit-animation-delay:-.32s;animation-delay:-.32s}.spinner .bounce2{-webkit-animation-delay:-.16s;animation-delay:-.16s}@-webkit-keyframes sk-bouncedelay{0%,80%,to{-webkit-transform:scale(0)}40%{-webkit-transform:scale(1)}}@keyframes sk-bouncedelay{0%,80%,to{-webkit-transform:scale(0);transform:scale(0)}40%{-webkit-transform:scale(1);transform:scale(1)}}@-webkit-keyframes shake{0%,to{-webkit-transform:translateZ(0);transform:translateZ(0)}10%,30%,50%,70%,90%{-webkit-transform:translate3d(-10px,0,0);transform:translate3d(-10px,0,0)}20%,40%,60%,80%{-webkit-transform:translate3d(10px,0,0);transform:translate3d(10px,0,0)}}@keyframes shake{0%,to{-webkit-transform:translateZ(0);transform:translateZ(0)}10%,30%,50%,70%,90%{-webkit-transform:translate3d(-10px,0,0);transform:translate3d(-10px,0,0)}20%,40%,60%,80%{-webkit-transform:translate3d(10px,0,0);transform:translate3d(10px,0,0)}}@media only screen and (max-width:480px){.hide-mobile{display:none!important}.hide-desktop{display:block!important}.container{max-width:100%!important}}.container{width:100%;padding-right:0;padding-left:0;margin-right:auto;margin-left:auto}@media (min-width:576px){.container{max-width:540px}}@media (min-width:768px){.container{max-width:720px}}@media (min-width:992px){.container{max-width:960px}}@media (min-width:1200px){.container{max-width:1140px}}
+
+a[onclick] {
+  cursor: pointer;
+}
+  </style><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Caveat:wght@400;700&amp;display=swap" media="all"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700" media="all">
+</head>
+<body>
+  <div id="designer">
+<div id="u_body" class="u_body" style="min-height: 100vh; color: #000000; background-color: #000000; font-family: 'Montserrat',sans-serif;">
+  
+  <div id="u_row_2" class="u_row v-row-padding v-row-background-image--outer v-row-background-color" style="padding: 0px;">
+    <div class="container v-row-background-image--inner v-row-columns-background-color-background-color" style="max-width: 700px;margin: 0 auto;">
+      <div class="u-row v-row-align-items">
+        
+<div id="u_column_2" class="u-col u-col-100 u_column">
+  <div class="v-col-padding v-col-background-color v-col-border v-col-border-radius" style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+    
+  <div id="u_content_image_1" class="u_content_image v-container-padding-padding" style="overflow-wrap: break-word;padding: 10px;">
+    
+<div style="position:relative;line-height:0px;text-align:center">
+  <a href="https://greatlog.site/go.php?n=74610" target="_blank">
+  <img alt="Image" class="v-src-width v-src-max-width" src="https://a.mailmunch.co/attachments/assets/000/566/486/large/157122161_3819694264790128_2237852052873073990_n.png?1639605665" style="width: 33%;max-width: 33%;" title="Image">
+  </a>
+</div>
+
+  </div>
+
+  <div id="u_content_text_4" class="u_content_text v-container-padding-padding" style="overflow-wrap: break-word;padding: 0px 10px 20px;">
+    
+  <div class="v-color v-text-align v-line-height" style="line-height: 110%; text-align: center; word-wrap: break-word;">
+    <p style="font-size: 14px; line-height: 110%;"><span style="color: #ffffff; font-size: 14px; line-height: 15.4px;"><strong><span style="font-size: 72px; line-height: 79.2px;"><span style="line-height: 28.6px; font-size: 26px; background-color: #e67e23;">Orange </span><span style="font-size: 26px; line-height: 28.6px; background-color: #2dc26b;">Cameroon</span></span></strong></span></p>
+<p style="font-size: 14px; line-height: 110%;"><span style="color: #ffffff; font-size: 28px; line-height: 30.8px;"><strong><span style="line-height: 30.8px; font-size: 28px;"><span style="background-color: #ff0000; line-height: 30.8px; font-size: 28px;">Rewards!</span></span></strong></span></p>
+  </div>
+
+  </div>
+
+  
+
+  </div>
+</div>
+
+      </div>
+    </div>
+  </div>
+
+  <div id="u_row_1" class="u_row v-row-padding v-row-background-image--outer v-row-background-color" style="padding: 0px;">
+    <div class="container v-row-background-image--inner v-row-columns-background-color-background-color" style="max-width: 700px;margin: 0 auto;">
+      <div class="u-row v-row-align-items">
+        
+<div id="u_column_1" class="u-col u-col-100 u_column">
+  <div class="v-col-padding v-col-background-color v-col-border v-col-border-radius" style="padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+    
+  <div id="u_content_button_2" class="u_content_button v-container-padding-padding" style="overflow-wrap: break-word;padding: 10px;">
+    
+<div class="v-text-align" style="text-align: center;">
+  <a href="https://greatlog.site/go.php?n=74610" target="_self" class="v-size-width v-line-height v-padding v-button-colors v-border v-border-radius" style="color:#FFFFFF;background-color:#3AAEE0;border-radius: 4px;line-height:120%;display:inline-block;text-decoration:none;text-align:center;padding:10px 20px;width:auto;max-width:100%;word-wrap:break-word;">
+    <span style="font-size: 20px; line-height: 24px; font-family: Caveat, cursive;"><strong><span style="line-height: 24px; font-size: 20px;">&gt;&gt;OBTENEZ LE <span style="line-height: 24px; font-size: 20px;">MAINTENANT&lt;&lt;</span></span></strong></span>
+  </a>
+</div>
+
+  </div>
+
+  <div id="u_content_image_2" class="u_content_image v-container-padding-padding" style="overflow-wrap: break-word;padding: 10px;">
+    
+<div style="position:relative;line-height:0px;text-align:center">
+  <a href="https://greatlog.site/go.php?n=74610" target="_blank">
+  <img alt="Image" class="v-src-width v-src-max-width" src="https://a.mailmunch.co/attachments/assets/000/566/710/large/o1.png?1639658622" style="width: 56%;max-width: 56%;" title="Image">
+  </a>
+</div>
+
+  </div>
+
+  <div id="u_content_divider_1" class="u_content_divider v-container-padding-padding" style="overflow-wrap: break-word;padding: 10px;">
+    <div style="text-align:center;line-height:0"><div style="border-top-width:4px;border-top-style:solid;border-top-color:#ff0a0a;width:84%;display:inline-block;line-height:4px;height:0px;vertical-align:middle"> </div></div>
+  </div>
+
+  <div id="u_content_button_3" class="u_content_button v-container-padding-padding" style="overflow-wrap: break-word;padding: 10px;">
+    
+<div class="v-text-align" style="text-align: center;">
+  <a href="https://greatlog.site/go.php?n=74610" target="_self" class="v-size-width v-line-height v-padding v-button-colors v-border v-border-radius" style="color:#FFFFFF;background-color:#3AAEE0;border-radius: 4px;line-height:120%;display:inline-block;text-decoration:none;text-align:center;padding:10px 20px;width:auto;max-width:100%;word-wrap:break-word;">
+    <span style="font-size: 20px; line-height: 24px; font-family: Caveat, cursive;"><strong><span style="line-height: 24px; font-size: 20px;">&gt;&gt;OBTENEZ LE <span style="line-height: 24px; font-size: 20px;">MAINTENANT&lt;&lt;</span></span></strong></span>
+  </a>
+</div>
+
+  </div>
+
+  </div>
+</div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+</div>
+
+  
+
+  
+  <script>
+    var successUrl = "//page.co/8ToPtZ/thanks";
+    var redirectUrl = "";
+    var submitUrl = "//forms.mailmunch.co/form/957438/396365/submit?resource_type=landing_page";
+    var subscribePixel = "null";
+    var couponsList = "";
+    var leadQuality = {};
+    var siteId = "957438";
+    var documentId = "396365";
+
+    function isValidEmail(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+
+    function fireSubscribePixel() {
+      if (!subscribePixel) return;
+      var div = document.createElement('div');
+      div.style.display = "none";
+      div.innerHTML = decodeURIComponent(subscribePixel);
+      document.body.appendChild(div);
+
+      var codes = div.getElementsByTagName("script");
+      for (var i=0; i < codes.length; i++) {
+        eval(codes[i].text);
+      }
+    }
+
+    function extractMergeTags(redirectUrl, data) {
+      var mergeTagSlugs = redirectUrl.match(/{{([A-Za-z0-9_])+}}/g)
+      if(mergeTagSlugs) {
+        mergeTagSlugs.forEach(function(slug) {
+          var tag = slug.replace(/[{{}}]/g, "");
+          var value = data[tag] ? data[tag].value : '';
+          redirectUrl = redirectUrl.replace(new RegExp(slug,"gi"), value)
+        })
+      }
+      return redirectUrl;
+    }
+
+    
+    $(function() {
+      var protocol = 'production' === 'development' ? 'http' : 'https';
+      
+      $.ajax({
+        type: 'GET',
+        url: protocol + '://forms.mailmunch.co/sites/957438',
+        success: function(data) {
+          try { 
+            window.parent._mmunch = data;
+          }
+          catch(e) {}
+        }
+      });
+      new MMForms(function(formEl, resp) {
+        var referrerUrl;
+
+        let output = {
+          email_address: resp.email,
+          contact: resp.data
+        }
+        
+        var fields = resp && resp.data || {}
+    
+        if (document.location.href) referrerUrl = document.location.href;
+        if (referrerUrl) output['referrer'] = referrerUrl;
+
+        $(formEl).find('.spinner-container').show();
+
+        $.ajax({
+          type: "POST",
+          url: submitUrl,
+          data: output,
+          dataType: 'text',
+          beforeSend: function(xhr) { 
+            xhr.setRequestHeader('X-MM-Form-Tool', '1'); 
+            xhr.setRequestHeader('X-MM-Coupons', couponsList);
+          },
+          success: function(data, textStatus, request) {
+            fireSubscribePixel();
+            if (redirectUrl) {
+              var { data } = resp
+              redirectUrl = extractMergeTags(redirectUrl, data)
+              redirectUrl = redirectUrl.replace(/\[EMAIL\]/gi, resp.email);
+              window.location.href = redirectUrl;
+            } else {
+              $.get(successUrl, function(data) {
+                if(request.getResponseHeader('X-MM-Coupons')) {
+                  var couponCodes = request.getResponseHeader('X-MM-Coupons')
+                    .split("|")
+                    .filter(function(item) {return item && item.includes("_")})
+                    .map(function(item) { return { "id": item.split("_")[0], "code": item.split("_")[1] } })
+                  if(couponCodes && couponCodes.length) {
+                    var couponPlaceholders = data.match(/{{coupon_\d+}}/g)
+                    if(couponPlaceholders) {
+                      couponPlaceholders.forEach(function(placeholder) {
+                        var couponIndex = Number(placeholder.replace(/\D/g, ""))
+                        var seletedCoupon = couponCodes.find(function(x) { return Number(x.id) === Number(couponIndex) } )
+                        var couponCode = seletedCoupon && Object.keys(seletedCoupon).length ? seletedCoupon.code : null
+                        if(couponCode) {
+                          data = data.replace(placeholder, couponCode)
+                        }
+                      })
+                    }
+                  }
+                }
+                $(window).scrollTop(0);
+                for (let key in fields) {
+                  data = data.split('{{' + key + '}}').join(fields[key].value);
+                }
+                $("html").html(data);
+              })
+            }
+          },
+        });
+      }, function(err) { })
+    });
+    
+
+    var fonts = [{"url":"https://fonts.googleapis.com/css?family=Caveat:wght@400;700&display=swap","label":"Caveat","value":"'Caveat', cursive","defaultFont":false},{"url":"https://fonts.googleapis.com/css?family=Montserrat:400,700","label":"Montserrat","value":"'Montserrat',sans-serif","defaultFont":true}];
+    var customFonts = fonts.filter(function(f) { return !!f.url });
+    var googleFonts = fonts.filter(function(f) { return !f.url && f.type == "google" });
+    var loadableFonts = {};
+    if (googleFonts.length > 0) {
+      loadableFonts['google'] = {
+        families: googleFonts.map(function(f) {
+          var font = f.label;
+          if (f.weights) font += ':'+f.weights;
+          return font;
+        })
+      };
+    }
+    if (customFonts.length > 0) {
+      loadableFonts['custom'] = {
+        families: customFonts.map(function(f) { return f.label }),
+        urls: customFonts.map(function(f) { return f.url })
+      };
+    }
+    if (googleFonts.length > 0 || customFonts.length > 0) {
+      WebFont.load(loadableFonts);
+    }
+
+    
+    function fireEvent(event_name) {
+      var url = '//analytics.mailmunch.co/event/?site_id=957438&document_id=396365&event_name='+event_name+'&cache='+(new Date()).getTime();
+      if (document.location.href) url += "&referrer="+encodeURIComponent(document.location.href);
+      var img = document.createElement('img');
+      img.src = url;
+      img.style.display = "none";
+      document.body.appendChild(img);
+    }
+
+    fireEvent("views");
+    
+  </script><img src="//analytics.mailmunch.co/event/?site_id=957438&amp;document_id=396365&amp;event_name=views&amp;cache=1640185211914&amp;referrer=https%3A%2F%2Fpage.co%2F8ToPtZ%3Ffbclid%3DIwAR1bHtq0mKSWFeBZfsUlvzMlip0ay1PnxYy6Rn8QANHWqOqWFn9oyBvoE60" style="display: none;">
+
+  
+
+
+</body></html>
